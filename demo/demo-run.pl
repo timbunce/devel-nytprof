31db99842a1ad4342f97064fe6def1bb12cd09bc
Tim Bunce <Tim.Bunce@pobox.com>
2008-07-07 14:54:10 +0000

    Update demo script to run from top directory
    git-svn-id: https://perl-devel-nytprof.googlecode.com/svn/trunk@246 cc63bb4f-3149-0410-a34e-ff7ca2d9011c

--- demo/demo-run.pl
+++ demo/demo-run.pl
@@ -4,19 +4,6 @@ use strict;
 my %runs = (
     plain => {
     },
-    subs => {
-        NYTPROF => 'blocks',
-        NYTPROF_DATASET => 'fid_sub_time',
-    },
-    blocks => {
-        NYTPROF => 'blocks',
-        NYTPROF_DATASET => 'fid_block_time',
-    },
-    blocks2 => {
-        NYTPROF => 'blocks',
-        NYTPROF_DATASET => 'fid_block_time',
-        NYTPROF_HTML => 1,
-    },
 );
 
 
@@ -24,15 +11,16 @@ for my $run (keys %runs) {
 
     $ENV{NYTPROF}      = $runs{$run}{NYTPROF} || '';
     $ENV{NYTPROF_HTML} = $runs{$run}{NYTPROF_HTML} || '';
-    $ENV{NYTPROF_DATASET} = $runs{$run}{NYTPROF_DATASET} || '';
 
-    system("perl -Mblib -MDevel::NYTProf demo-code.pl @ARGV") == 0
+    system("perl -Mblib -MDevel::NYTProf demo/demo-code.pl @ARGV") == 0
         or exit 0;
 
-    system("perl -Mblib bin/nytprofhtml -out=profiler-$run") == 0
+    system("rm -rf demo/profiler-$run") == 0
+        or exit 0;
+    system("perl -Mblib bin/nytprofhtml -out=demo/profiler-$run") == 0
         or exit 0;
 
-    system "open profiler-$run/index.html"
+    system "open demo/profiler-$run/index.html"
         if $^O eq 'darwin';
 
     sleep 1;
