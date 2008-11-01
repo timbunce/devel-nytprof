edff6c92dc69bea360bae08ecad9a4dc0091979d
Tim Bunce <Tim.Bunce@pobox.com>
2008-11-01 08:06:13 +0000

    Bump version to 2.07 (because PAUSE indexed the 2.06_RC1 release I made for cpantesters) Include t/test20-streval.x that I'd missed previously.
    git-svn-id: https://perl-devel-nytprof.googlecode.com/svn/trunk@583 cc63bb4f-3149-0410-a34e-ff7ca2d9011c

--- lib/Devel/NYTProf/Data.pm
+++ lib/Devel/NYTProf/Data.pm
@@ -50,7 +50,7 @@ use Scalar::Util qw(blessed);
 use Devel::NYTProf::Core;
 use Devel::NYTProf::Util qw(strip_prefix_from_paths get_abs_paths_alternation_regex);
 
-our $VERSION = '2.06';
+our $VERSION = '2.07';
 
 my $trace = (($ENV{NYTPROF}||'') =~ m/\b trace=(\d+) /x) && $1; # XXX a hack
 
