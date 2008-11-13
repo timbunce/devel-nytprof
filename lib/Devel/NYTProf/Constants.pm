d1198cbcd85c69f59a6875713a0f182e8a8e3b9c
Tim Bunce <Tim.Bunce@pobox.com>
2008-11-13 21:15:32 +0000

    Add Exporter, even though it worked just fine without it.
    git-svn-id: https://perl-devel-nytprof.googlecode.com/svn/trunk@603 cc63bb4f-3149-0410-a34e-ff7ca2d9011c

--- lib/Devel/NYTProf/Constants.pm
+++ lib/Devel/NYTProf/Constants.pm
@@ -4,6 +4,8 @@ use strict;
 
 use Devel::NYTProf::Core;
 
+use base 'Exporter';
+
 my $symbol_table = do { no strict; \%{"Devel::NYTProf::Constants::"} };
 
 our @EXPORT_OK = grep { /^NYTP_/ } keys %$symbol_table;
