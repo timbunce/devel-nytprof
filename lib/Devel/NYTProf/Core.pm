c0d491a9a0f42e67fe2559ecbba09a3156f85e51
Tim Bunce <Tim.Bunce@pobox.com>
2012-12-29 16:23:54 +0000

    Prep for 4.23

--- lib/Devel/NYTProf/Core.pm
+++ lib/Devel/NYTProf/Core.pm
@@ -14,7 +14,7 @@ package Devel::NYTProf::Core;
 
 use XSLoader;
 
-our $VERSION = '4.12';    # increment with XS changes too
+our $VERSION = '4.23';    # increment with XS changes too
 
 XSLoader::load('Devel::NYTProf', $VERSION);
 
