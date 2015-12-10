512de237f2d82307cc8b8d2d822d32c9f8e45b04
Denis Ibaev <dionys@gmail.com>
2015-12-10 20:56:57 +0300

    Changed URL of KCachegrind home page

--- lib/Devel/NYTProf.pm
+++ lib/Devel/NYTProf.pm
@@ -853,7 +853,7 @@ This is the main report generation tool for NYTProf.
 =head2 nytprofcg
 
 Translates a profile into a format that can be loaded into KCachegrind
-L<http://kcachegrind.sourceforge.net>
+L<http://kcachegrind.github.io/>
 
 =head2 nytprofcalls
 
