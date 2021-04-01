08e4bace027c7cddbd363016f0d9a8904c398b6e
James E Keenan <jkeenan@cpan.org>
2021-03-31 21:06:32 -0400

    Start to test previously unexercised subs in Data.pm
    Comment out one sub that is not used in current distribution (at least
    until we have a good reason to retain it).

--- lib/Devel/NYTProf/Data.pm
+++ lib/Devel/NYTProf/Data.pm
@@ -290,42 +290,43 @@ sub package_subinfo_map {
 #       "Foo::Baz::" => [ [ subinfo4 ] ]           # 1 sub in 1 pkg
 #   }
 # ]
-sub packages_at_depth_subinfo {
-    my $self = shift;
-    my ($opts) = @_;
-
-    my $merged = $opts->{merge_subinfos};
-    my $all_pkgs = $self->package_subinfo_map($merged) || {};
-
-    my @packages_at_depth = ({});
-    while ( my ($fullpkgname, $subinfos) = each %$all_pkgs ) {
-
-        $subinfos = [ grep { $_->calls } @$subinfos ]
-            if not $opts->{include_unused_subs};
-
-        next unless @$subinfos;
-
-        my @parts = split /::/, $fullpkgname; # drops empty trailing part
-
-        # accumulate @$subinfos for the full package name
-        # and also for each successive truncation of the package name
-        for (my $depth; $depth = @parts; pop @parts) {
-            my $pkgname = join('::', @parts, '');
-
-            my $store = ($merged) ? $subinfos->[0] : $subinfos;
-
-            # { "Foo::" => [ [sub1,sub2], [sub3,sub4] ] } # subs from 2 packages
-            my $pkgdepthinfo = $packages_at_depth[$depth] ||= {};
-            push @{ $pkgdepthinfo->{$pkgname} }, $store;
-
-            last if not $opts->{rollup_packages};
-        }
-    }
-    # fill in any undef holes at depths with no subs
-    $_ ||= {} for @packages_at_depth;
-
-    return \@packages_at_depth;
-}
+# 2021-03-31: not found in t/ lib/ bin/
+#sub packages_at_depth_subinfo {
+#    my $self = shift;
+#    my ($opts) = @_;
+#
+#    my $merged = $opts->{merge_subinfos};
+#    my $all_pkgs = $self->package_subinfo_map($merged) || {};
+#
+#    my @packages_at_depth = ({});
+#    while ( my ($fullpkgname, $subinfos) = each %$all_pkgs ) {
+#
+#        $subinfos = [ grep { $_->calls } @$subinfos ]
+#            if not $opts->{include_unused_subs};
+#
+#        next unless @$subinfos;
+#
+#        my @parts = split /::/, $fullpkgname; # drops empty trailing part
+#
+#        # accumulate @$subinfos for the full package name
+#        # and also for each successive truncation of the package name
+#        for (my $depth; $depth = @parts; pop @parts) {
+#            my $pkgname = join('::', @parts, '');
+#
+#            my $store = ($merged) ? $subinfos->[0] : $subinfos;
+#
+#            # { "Foo::" => [ [sub1,sub2], [sub3,sub4] ] } # subs from 2 packages
+#            my $pkgdepthinfo = $packages_at_depth[$depth] ||= {};
+#            push @{ $pkgdepthinfo->{$pkgname} }, $store;
+#
+#            last if not $opts->{rollup_packages};
+#        }
+#    }
+#    # fill in any undef holes at depths with no subs
+#    $_ ||= {} for @packages_at_depth;
+#
+#    return \@packages_at_depth;
+#}
 
 sub all_fileinfos {
     my @all = @{shift->{fid_fileinfo}};
