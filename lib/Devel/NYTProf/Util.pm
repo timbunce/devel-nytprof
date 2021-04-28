5184c0aa331ec76917eb88e2690fe1b119a0dd27
James E Keenan <jkeenan@cpan.org>
2021-04-28 21:54:23 +0000

    Util.pm: Make regex more flexible
    Per suggestion by @shawnlaffan in
    https://github.com/timbunce/devel-nytprof/issues/170.

--- lib/Devel/NYTProf/Util.pm
+++ lib/Devel/NYTProf/Util.pm
@@ -167,7 +167,7 @@ sub fmt_float {
         # But our exponents will always be e-05 to e-09, never e-10 or smaller
         # so remove the leading zero to make these small numbers stand out less
         # on the table.
-        $val =~ s/e-0/e-/;
+        $val =~ s/e-0+/e-/;
     }
     elsif ($val != int($val)) {
         $val = sprintf("%.${precision}f", $val);
